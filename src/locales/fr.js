31c00cbfeba58560673b59018269c997a5144ae3
Francois BOCQUET <fbocquet@gmail.com>
2020-08-11 20:33:54 +0200

    Create fr.js

---
+++ src/locales/fr.js
@@ -0,0 +1,16 @@
+export default {
+  share_button: 'Partager avec les élèves',
+  send_message: 'Envoyer un message',
+  what_name: 'Quel est votre nom ?',
+  student: 'Elève',
+  teacher: 'Enseignant',
+  set_name: 'Ajouter votre nom',
+  share: {
+    title: 'Partager',
+    link_info: 'Veuillez donner ce lien à vos élèves pour qu'ils participent :',
+    button_copy: 'Copier le lien',
+    qr_info: 'Vous pouvez également scanner ce QR Code à l'aide d'un appareil photo mobile :',
+    feedback: 'Pour tout commentaire, écrivez à <a href="mailto:info@peer.school">info@peer.school</a>',
+    media_server: 'Utiliser un serveur de médias'
+  }
+}
